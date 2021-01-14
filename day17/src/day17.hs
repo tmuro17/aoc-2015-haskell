c19fea90b848477268dc7fcf04bc0f266b1e46c3
Tanner Muro <tmuro17@gmail.com>
2021-01-13 23:51:21 -0500

    Fix up day17

--- day17/src/day17.hs
+++ day17/src/day17.hs
@@ -10,19 +10,34 @@ import Test.Tasty.Hspec
 
 main :: IO ()
 main = do
+    spc <- spec
+    tSpec <- testSpec "day17" spc
+    defaultMain (testGroup "Tests" [tSpec])
+
+spec :: IO (SpecWith ())
+spec = do
     text <- TIO.readFile "data/input17.txt"
     lns <- pure $ T.lines text
     strs <- pure $ map T.unpack lns
-    print $ solvePart1 strs
-    print $ solvePart2 strs
+    return $ context "spec" $ do
+        describe "overall" $ do
+            it "passes 17a" $
+                solvePart1 strs
+                `shouldBe`
+                4372
+            it "passes 17b" $
+                solvePart2 strs
+                `shouldBe`
+                4
 
 solvePart1 :: [String] -> Int
 solvePart1 = length . filter (== 150) . map sum . subsequences . map read
 
 solvePart2 :: [String] -> Int
-solvePart2 = length . ap (filter . (==) . minimum . map length . filter ((150 ==) . sum) . subsequences . map read) (map length . filter ((150 ==) . sum) . subsequences . map read)
---solvePart2 s = length $ filter (== minNum) $ lengths
---    where
---        lengths = map length $ filter ((== 150) . sum) $ subsequences $ map read s
---        minNum = minimum $ map length $ filter ((== 150) . sum) $ subsequences $ map read s
+solvePart2 s = length $ filter (== minNum) $ lengths
+    where
+        lengths = map length $ filter ((== 150) . sum) $ subsequences $ map read s
+        minNum = minimum $ lengths
+-- Pointfree is cool but the above is lazier and therefore twice as fast
+--solvePart2 = length . ap (filter . (==) . minimum . map length . filter ((150 ==) . sum) . subsequences . map read) (map length . filter ((150 ==) . sum) . subsequences . map read)
 
